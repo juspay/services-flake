1a0cd58b0839aacad2b949d20c183ee7b3e7d421
Sridhar Ratnakumar <srid@srid.ca>
2023-06-21 10:00:35 -0400

    test.sh: Fix test to use current flake

--- test.sh
+++ test.sh
@@ -5,5 +5,5 @@ cd "$(dirname "$0")"
 # On NixOS, run the VM tests to test runtime behaviour
 if command -v nixos-rebuild &> /dev/null; then
   # service tests
-  nix flake check -L ./test
+  nix flake check -L ./test --override-input services-flake .
 fi
