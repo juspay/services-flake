8009e252f39096000cad4b0aea1f0ddc84255987
shivaraj-bh <sbh69840@gmail.com>
2023-07-04 20:17:52 +0530

    decouple hbaConfFile and run nixpkgs-fmt

--- nix/postgres_test.nix
+++ nix/postgres_test.nix
@@ -3,16 +3,16 @@
     enable = true;
     listen_addresses = "127.0.0.1";
   };
-  testScript = 
-  let
-    psql = "${config.services.postgres.package}/bin/psql";
-  in
-  ''
-    process_compose.wait_until(lambda procs:
-      # TODO: Check for 'ready'
-      procs["postgres"]["status"] == "Running"
-    )
-    machine.succeed("echo 'SELECT version();' | ${config.services.postgres.package}/bin/psql -h 127.0.0.1 -U tester")
-    machine.succeed("echo 'SHOW hba_file;' | ${psql} -h 127.0.0.1 -U tester | ${pkgs.gawk}/bin/awk 'NR==3' | ${pkgs.gnugrep}/bin/grep '^ /nix/store'")
-  '';
+  testScript =
+    let
+      psql = "${config.services.postgres.package}/bin/psql";
+    in
+    ''
+      process_compose.wait_until(lambda procs:
+        # TODO: Check for 'ready'
+        procs["postgres"]["status"] == "Running"
+      )
+      machine.succeed("echo 'SELECT version();' | ${config.services.postgres.package}/bin/psql -h 127.0.0.1 -U tester")
+      machine.succeed("echo 'SHOW hba_file;' | ${psql} -h 127.0.0.1 -U tester | ${pkgs.gawk}/bin/awk 'NR==3' | ${pkgs.gnugrep}/bin/grep '^ /nix/store'")
+    '';
 }
