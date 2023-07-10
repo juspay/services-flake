0f05eac04ea4e7ed4385656bf4911425bcc55719
Sridhar Ratnakumar <srid@srid.ca>
2023-07-10 12:54:38 -0400

    dev flake: check example and tests
    It makes sense to consolidate all of this to in one place, the "dev flake".

--- dev/flake.nix
+++ dev/flake.nix
@@ -1,9 +1,18 @@
+# Development flake
+#
+# We setup a dev environment as well as run tests (flake checks) here, such that
+# the top-level flake is simple enough for users.
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-root.url = "github:srid/flake-root";
     mission-control.url = "github:Platonic-Systems/mission-control";
+
+    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
+    services-flake.url = "path:..";
+    example.url = "path:../example";
+
   };
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
@@ -11,8 +20,28 @@
       imports = [
         inputs.flake-root.flakeModule
         inputs.mission-control.flakeModule
+        inputs.process-compose-flake.flakeModule
       ];
-      perSystem = { pkgs, lib, config, ... }: {
+      perSystem = { inputs', pkgs, lib, config, ... }: {
+        # Test /example
+        checks = inputs'.example.checks;
+
+        # Test the individual services
+        process-compose = {
+          postgres = {
+            imports = [
+              inputs.services-flake.processComposeModules.default
+              ../nix/postgres_test.nix
+            ];
+          };
+          redis = {
+            imports = [
+              inputs.services-flake.processComposeModules.default
+              ../nix/redis_test.nix
+            ];
+          };
+        };
+
         mission-control.scripts = {
           ex = {
             description = "Run example";
