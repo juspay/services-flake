1649d9c8147cfdf82651205dd4b9ac3b3a845d52
shivaraj-bh <sbh69840@gmail.com>
2023-07-10 18:35:09 +0530

    nixos test for before and after script

--- nix/postgres_test.nix
+++ nix/postgres_test.nix
@@ -2,12 +2,19 @@
   services.postgres = {
     enable = true;
     listen_addresses = "127.0.0.1";
+    initialScript.before = "CREATE USER bar;";
+    initialScript.after = "CREATE DATABASE foo OWNER bar;";
   };
-  testScript = ''
+  testScript = let pg = config.services.postgres.package; in
+  ''
     process_compose.wait_until(lambda procs:
       # TODO: Check for 'ready'
       procs["postgres"]["status"] == "Running"
     )
-    machine.succeed("echo 'SELECT version();' | ${config.services.postgres.package}/bin/psql -h 127.0.0.1 -U tester")
+    machine.succeed("echo 'SELECT version();' | ${pg}/bin/psql -h 127.0.0.1 -U tester")
+    # initialScript.before test
+    machine.succeed("echo 'SELECT 1 FROM pg_roles WHERE rolname = \'bar\';' | ${pg}/bin/psql -h 127.0.0.1 -U tester | grep -q 1")
+    # initialScript.after test
+    machine.succeed("echo 'SELECT 1 FROM pg_database WHERE datname = \'foo\';' | ${pg}/bin/psql -h 127.0.0.1 -U tester | grep -q 1")
   '';
 }
