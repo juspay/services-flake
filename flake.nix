687ea2adb54dd31b26e569d4e8c372989fc9b306
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2023-07-10 21:25:49 -0400

    Add `nixci` config to flake (#19)

--- flake.nix
+++ flake.nix
@@ -6,5 +6,21 @@
       description = "Example flake using process-compose-flake";
       path = builtins.path { path = ./example; filter = path: _: baseNameOf path == "flake.nix"; };
     };
+
+    # Config for https://github.com/srid/nixci
+    # To run this, `nix run github:srid/nixci`
+    nixci = let overrideInputs = { "services-flake" = ./.; }; in {
+      example = {
+        inherit overrideInputs;
+        dir = "./example";
+      };
+      test = {
+        inherit overrideInputs;
+        dir = "./test";
+      };
+      dev = {
+        dir = "./dev";
+      };
+    };
   };
 }
