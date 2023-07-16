179af31b82c0fb92c798523087b6dc46c84b5472
shivaraj-bh <sbh69840@gmail.com>
2023-07-17 00:37:53 +0530

    hello: init multiple-services

--- nix/hello.nix
+++ nix/hello.nix
@@ -1,24 +1,37 @@
-{ pkgs, lib, config, ... }:
+{ pkgs, lib, options, config, ... }:
 
 {
-  options.services.hello = {
-    enable = lib.mkEnableOption "hello";
-    name = lib.mkOption {
-      type = lib.types.str;
-      default = "hello";
-      description = "Process name";
-    };
-    package = lib.mkPackageOption pkgs "hello" { };
-    greeting = lib.mkOption {
-      type = lib.types.str;
-      default = "Hello";
-      description = "The greeting to use";
-    };
+  options.services.hello = lib.mkOption {
+    type = with lib.types; attrsOf (submodule ({ name, config, ... }:
+      let serviceName = "hello-${name}"; in {
+        options = {
+          enable = lib.mkEnableOption "hello";
+          name = lib.mkOption {
+            type = lib.types.str;
+            default = "hello";
+            description = "Process name";
+          };
+          package = lib.mkPackageOption pkgs "hello" { };
+          greeting = lib.mkOption {
+            type = lib.types.str;
+            default = "Hello";
+            description = "The greeting to use";
+          };
+          output = lib.mkOption {
+            type = (options.settings.type.getSubOptions [ ]).processes.type;
+            internal = true;
+            readOnly = true;
+            default = {
+              "${serviceName}".command = ''
+                set -x
+                ${lib.getExe config.package} -g "${config.greeting}"
+              '';
+            };
+          };
+        };
+      }));
   };
-  config = let cfg = config.services.hello; in lib.mkIf cfg.enable {
-    settings.processes.${cfg.name}.command = ''
-      set -x
-      ${lib.getExe cfg.package} -g "${cfg.greeting}"
-    '';
+  config = {
+    settings.processes = lib.mkMerge (lib.mapAttrsToList (_: cfg: lib.mkIf cfg.enable cfg.output) config.services.hello);
   };
 }
