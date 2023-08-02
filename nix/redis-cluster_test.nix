8d3d76a104bd8c256a0ddffa9a74bbb1f7d0e58e
shivaraj-bh <sbh69840@gmail.com>
2023-08-02 09:53:11 +0530

    redis-cluster: change wait_until condition

--- nix/redis-cluster_test.nix
+++ nix/redis-cluster_test.nix
@@ -2,8 +2,8 @@
   services.redis-cluster."c1".enable = true;
   testScript = ''
     process_compose.wait_until(lambda procs:
-      # TODO: Check for 'is_ready' instead of 'status'
-      procs["c1-cluster-create"]["status"] == "Completed"
+      # TODO: Check for 'is_ready' of `c1-cluster-create` instead of `c1-n1`. This should be easy after https://github.com/juspay/services-flake/issues/32
+      procs["c1-n1"]["status"] == "Running"
     )
     machine.succeed("${config.services.redis-cluster.c1.package}/bin/redis-cli -p 30001 ping | grep -q 'PONG'")
     machine.succeed("${config.services.redis-cluster.c1.package}/bin/redis-cli -p 30002 ping | grep -q 'PONG'")
