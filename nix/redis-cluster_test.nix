aeace1eb8dd229e59cb7a62a8c57bca173ac254e
shivaraj-bh <sbh69840@gmail.com>
2023-08-02 09:55:33 +0530

    more context for todo

--- nix/redis-cluster_test.nix
+++ nix/redis-cluster_test.nix
@@ -2,7 +2,8 @@
   services.redis-cluster."c1".enable = true;
   testScript = ''
     process_compose.wait_until(lambda procs:
-      # TODO: Check for 'is_ready' of `c1-cluster-create` instead of `c1-n1`. This should be easy after https://github.com/juspay/services-flake/issues/32
+      # TODO: Check for 'is_ready' of `c1-cluster-create` instead of `c1-n1` (status of `c1-cluster-create` determines whether the hashslots are assigned). 
+      #       This should be easy after https://github.com/juspay/services-flake/issues/32
       procs["c1-n1"]["status"] == "Running"
     )
     machine.succeed("${config.services.redis-cluster.c1.package}/bin/redis-cli -p 30001 ping | grep -q 'PONG'")
