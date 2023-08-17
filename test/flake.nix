aac2944152f1476b4f9ceb8700ee84004eacebe3
shivaraj-bh <sbh69840@gmail.com>
2023-08-17 15:04:18 +0530

    set swagger port to 0

--- test/flake.nix
+++ test/flake.nix
@@ -15,21 +15,22 @@
       perSystem = { self', pkgs, lib, ... }: {
         process-compose = {
           postgres = {
-            port = 8080;
+            # TODO: remove `port = 0`; as it will be default after this: https://github.com/Platonic-Systems/process-compose-flake/pull/42
+            port = 0;
             imports = [
               inputs.services-flake.processComposeModules.default
               ../nix/postgres_test.nix
             ];
           };
           redis = {
-            port = 8081;
+            port = 0;
             imports = [
               inputs.services-flake.processComposeModules.default
               ../nix/redis_test.nix
             ];
           };
           redis-cluster = {
-            port = 8082;
+            port = 0;
             imports = [
               inputs.services-flake.processComposeModules.default
               ../nix/redis-cluster_test.nix
