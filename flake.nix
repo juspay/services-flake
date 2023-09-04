994406287dd6367d331fce62839a08accf44b0c4
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2023-09-04 17:05:05 -0400

    nixci: upgrade to 0.2

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
 
     # Config for https://github.com/srid/nixci
     # To run this, `nix run github:srid/nixci`
-    nixci = let overrideInputs = { "services-flake" = ./.; }; in {
+    nixci.default = let overrideInputs = { "services-flake" = ./.; }; in {
       example = {
         inherit overrideInputs;
         dir = "./example";
