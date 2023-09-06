1e21577dd9b9751b630d38e155a18a631d39f7b4
shivaraj-bh <sbh69840@gmail.com>
2023-09-06 14:44:50 +0530

    kafka-test: run zk on different port

--- nix/apache-kafka_test.nix
+++ nix/apache-kafka_test.nix
@@ -1,6 +1,9 @@
 { pkgs, config, ... }: {
   services.zookeeper."z1".enable = true;
+  # To avoid conflicting with `zookeeper_test.nix` in case the tests are run in parallel
+  services.zookeeper."z1".port = 2182;
   services.apache-kafka."k1".enable = true;
+  services.apache-kafka."k1".zookeeper = "localhost:2182";
   settings.processes.test =
     {
       command = pkgs.writeShellApplication {
