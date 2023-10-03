336d454d28d53d84b65e988edf2976216ac0c778
shivaraj-bh <sbh69840@gmail.com>
2023-10-03 16:55:08 +0530

    update process-compose-flake in test and example flake

--- example/flake.nix
+++ example/flake.nix
@@ -4,7 +4,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
-    process-compose-flake.url = "github:shivaraj-bh/process-compose-flake/namespace";
+    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
     services-flake.url = "github:juspay/services-flake";
 
     northwind.url = "github:pthom/northwind_psql";
