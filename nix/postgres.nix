1ffb2a7915404c43a87c31d70d31ba8f3a271e80
shivaraj-bh <sbh69840@gmail.com>
2023-12-23 16:03:56 +0530

    create postgres/setup-script.nix, move setupIntialDatabases to it

--- nix/postgres.nix
+++ nix/postgres.nix
@@ -271,50 +271,6 @@ in
             # DB initialization
             "${name}-init" =
               let
-                setupInitialDatabases =
-                  if config.initialDatabases != [ ] then
-                    (lib.concatMapStrings
-                      (database: ''
-                        echo "Checking presence of database: ${database.name}"
-                        # Create initial databases
-                        dbAlreadyExists=$(
-                          echo "SELECT 1 as exists FROM pg_database WHERE datname = '${database.name}';" | \
-                          psql -d postgres | \
-                          ${pkgs.gnugrep}/bin/grep -c 'exists = "1"' || true
-                        )
-                        echo $dbAlreadyExists
-                        if [ 1 -ne "$dbAlreadyExists" ]; then
-                          echo "Creating database: ${database.name}"
-                          echo 'create database "${database.name}";' | psql -d postgres
-
-
-                          ${lib.optionalString (database.schema != null) ''
-                          echo "Applying database schema on ${database.name}"
-                          if [ -f "${database.schema}" ]
-                          then
-                            echo "Running file ${database.schema}"
-                            ${pkgs.gawk}/bin/awk 'NF' "${database.schema}" | psql -d ${database.name}
-                          elif [ -d "${database.schema}" ]
-                          then
-                            # Read sql files in version order. Apply one file
-                            # at a time to handle files where the last statement
-                            # doesn't end in a ;.
-                            ls -1v "${database.schema}"/*.sql | while read f ; do
-                              echo "Applying sql file: $f"
-                              ${pkgs.gawk}/bin/awk 'NF' "$f" | psql -d ${database.name}
-                            done
-                          else
-                            echo "ERROR: Could not determine how to apply schema with ${database.schema}"
-                            exit 1
-                          fi
-                          ''}
-                        fi
-                      '')
-                      config.initialDatabases)
-                  else
-                    lib.optionalString config.createDatabase ''
-                     echo "CREATE DATABASE ''${USER:-$(id -nu)};" | psql -d postgres '';
-
                 runInitialScript =
                   let
                     scriptCmd = sqlScript: ''
@@ -338,7 +294,7 @@ in
                   else
                     toString value;
 
-
+                setupInitialDatabases = import ./postgres/setup-script.nix { inherit config pkgs lib; };
                 setupScript = pkgs.writeShellScriptBin "setup-postgres" ''
                   set -euo pipefail
                   export PATH=${postgresPkg}/bin:${pkgs.coreutils}/bin
@@ -350,7 +306,7 @@ in
 
                 '';
               in
-              { 
+              {
                 command = ''
                   export PGDATA="${config.dataDir}"
                   export PGHOST=$(readlink -f "${config.dataDir}")
@@ -371,7 +327,7 @@ in
 
                 configFile = pkgs.writeText "postgresql.conf" (lib.concatStringsSep "\n"
                   (lib.mapAttrsToList (n: v: "${n} = ${toStr v}") (config.defaultSettings // config.settings)));
-                
+
                 toStr = value:
                   if true == value then
                     "yes"
---
+++ nix/postgres/setup-script.nix
@@ -0,0 +1,47 @@
+{ config, pkgs, lib }:
+let
+  setupInitialDatabases =
+    if config.initialDatabases != [ ] then
+      (lib.concatMapStrings
+        (database: ''
+          echo "Checking presence of database: ${database.name}"
+          # Create initial databases
+          dbAlreadyExists=$(
+            echo "SELECT 1 as exists FROM pg_database WHERE datname = '${database.name}';" | \
+            psql -d postgres | \
+            ${pkgs.gnugrep}/bin/grep -c 'exists = "1"' || true
+          )
+          echo $dbAlreadyExists
+          if [ 1 -ne "$dbAlreadyExists" ]; then
+            echo "Creating database: ${database.name}"
+            echo 'create database "${database.name}";' | psql -d postgres
+
+
+            ${lib.optionalString (database.schema != null) ''
+            echo "Applying database schema on ${database.name}"
+            if [ -f "${database.schema}" ]
+            then
+              echo "Running file ${database.schema}"
+              ${pkgs.gawk}/bin/awk 'NF' "${database.schema}" | psql -d ${database.name}
+            elif [ -d "${database.schema}" ]
+            then
+              # Read sql files in version order. Apply one file
+              # at a time to handle files where the last statement
+              # doesn't end in a ;.
+              ls -1v "${database.schema}"/*.sql | while read f ; do
+                echo "Applying sql file: $f"
+                ${pkgs.gawk}/bin/awk 'NF' "$f" | psql -d ${database.name}
+              done
+            else
+              echo "ERROR: Could not determine how to apply schema with ${database.schema}"
+              exit 1
+            fi
+            ''}
+          fi
+        '')
+        config.initialDatabases)
+    else
+      lib.optionalString config.createDatabase ''
+        echo "CREATE DATABASE ''${USER:-$(id -nu)};" | psql -d postgres '';
+in
+setupInitialDatabases
