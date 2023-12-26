b951f67596f6b5aaf653f2c92e99312f1fb63e73
Rohit Singh <rsrohitsingh682@gmail.com>
2023-12-26 15:07:43 +0530

    add test

--- nix/postgres/postgres_test.nix
+++ nix/postgres/postgres_test.nix
@@ -4,6 +4,7 @@
     listen_addresses = "127.0.0.1";
     initialScript.before = "CREATE USER bar;";
     initialScript.after = "CREATE DATABASE foo OWNER bar;";
+    initialDumps = [ ./test.sql ];
   };
   settings.processes.test =
     let
@@ -21,6 +22,9 @@
 
           # initialScript.after test
           echo "SELECT 1 FROM pg_database WHERE datname = 'foo';" | psql -h 127.0.0.1 | grep -q 1
+
+          #intialDumps test
+          echo "SELECT * from users where user_name = 'test_user';" | psql -h 127.0.0.1 -d postgres | grep -q test_user
         '';
         name = "postgres-test";
       };
