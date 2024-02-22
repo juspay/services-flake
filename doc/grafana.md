625f2070346f1a6d11d5c482c0c987010c3ba2fc
Abhishek Singh <abhishek17513@gmail.com>
2024-02-22 22:25:51 +0530

    add grafana service (#108)

---
+++ doc/grafana.md
@@ -0,0 +1,40 @@
+# Grafana
+
+[Grafana open source](https://grafana.com/docs/grafana/latest/) is open source visualization and analytics software. It allows you to query, visualize, alert on, and explore your metrics, logs, and traces no matter where they are stored. It provides you with tools to turn your time-series database (TSDB) data into insightful graphs and visualizations.
+
+## Getting Started
+
+```nix
+# In `perSystem.process-compose.<name>`
+{
+  services.grafana."gf1".enable = true;
+}
+```
+
+{#tips}
+## Tips & Tricks
+
+{#change-database}
+### Changing Grafana database
+
+By default, Grafana stores data in the `sqlite3` [database](https://grafana.com/docs/grafana/latest/setup-grafana/configure-grafana/#database). It also supports `mysql` and `postgres`.
+
+To change the database to `postgres`, we can use the following config:
+
+```nix
+    services.postgres.pg1 = {
+      enable = true;
+      listen_addresses = "127.0.0.1";
+      initialScript.after = "CREATE USER root SUPERUSER;";
+    };
+    services.grafana.gf1 = {
+      enable = true;
+      extraConf.database = with config.services.postgres.pg1; {
+        type = "postgres";
+        host = "${listen_addresses}:${builtins.toString port}";
+        name = "postgres"; # database name
+      };
+    };
+    settings.processes."gf1".depends_on."pg1".condition = "process_healthy";
+    };
+```
---
+++ nix/grafana.nix
@@ -0,0 +1,99 @@
+{ pkgs, lib, name, config, ... }:
+let
+  inherit (lib) types;
+  iniFormat = pkgs.formats.ini { };
+in
+{
+  options = {
+    description = ''
+      Configure grafana.
+    '';
+    enable = lib.mkEnableOption name;
+
+    package = lib.mkPackageOption pkgs "grafana" { };
+
+    http_port = lib.mkOption {
+      type = types.int;
+      description = "Which port to run grafana on.";
+      default = 3000;
+    };
+
+    domain = lib.mkOption {
+      type = types.str;
+      description = "The public facing domain name used to access grafana from a browser.";
+      default = "localhost";
+    };
+
+    protocol = lib.mkOption {
+      type = types.str;
+      description = "Protocol (http, https, h2, socket).";
+      default = "http";
+    };
+
+    dataDir = lib.mkOption {
+      type = types.str;
+      description = "Directory where grafana stores its logs and data.";
+      default = "./data/${name}";
+    };
+
+    extraConf = lib.mkOption {
+      type = iniFormat.type;
+      description = "Extra configuration for grafana.";
+      default = { };
+      example = ''
+        {
+          security.admin_user = "patato";
+          security.admin_password = "potato";
+        }
+      '';
+    };
+
+    outputs.settings = lib.mkOption {
+      type = types.deferredModule;
+      internal = true;
+      readOnly = true;
+      default = {
+        processes."${name}" =
+          let
+            grafanaConfig = lib.recursiveUpdate
+              {
+                server = {
+                  inherit (config) protocol http_port domain;
+                };
+              }
+              config.extraConf;
+            grafanaConfigIni = iniFormat.generate "defaults.ini" grafanaConfig;
+            startScript = pkgs.writeShellApplication {
+              name = "start-grafana";
+              runtimeInputs = [ config.package ];
+              text = ''
+                grafana server --config ${grafanaConfigIni} \
+                               --homepath ${config.package}/share/grafana \
+                               cfg:paths.data="$(readlink -m ${config.dataDir})"
+              '';
+            };
+          in
+          {
+            command = startScript;
+            readiness_probe = {
+              http_get = {
+                host = config.domain;
+                scheme = config.protocol;
+                port = config.http_port;
+                path = "/api/health";
+              };
+              initial_delay_seconds = 15;
+              period_seconds = 10;
+              timeout_seconds = 2;
+              success_threshold = 1;
+              failure_threshold = 5;
+            };
+            namespace = name;
+
+            # https://github.com/F1bonacc1/process-compose#-auto-restart-if-not-healthy
+            availability.restart = "on_failure";
+          };
+      };
+    };
+  };
+}
