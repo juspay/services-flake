28fa4b9efb948494ce63d9328c01052513151d83
Abhishek Singh <abhishek17513@gmail.com>
2024-03-02 23:57:17 +0530

    cassandra_test: change package to cassandra_4

--- nix/cassandra_test.nix
+++ nix/cassandra_test.nix
@@ -1,5 +1,10 @@
 { pkgs, config, ... }: {
-  services.cassandra."cass1".enable = true;
+  services.cassandra."cass1" = {
+    enable = true;
+    # support for aarch64-darwin was added in cassandra-4
+    # https://github.com/apache/cassandra/blob/a87055d56a33a9b17606f14535f48eb461965b82/CHANGES.txt#L192
+    package = pkgs.cassandra_4;
+  };
 
   settings.processes.test =
     let
