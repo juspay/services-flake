3373bb0b3dc704f339290bb680a14526006d9793
Abhishek Singh <abhishek17513@gmail.com>
2024-03-03 13:05:28 +0530

    update cassandra_test

--- nix/cassandra_test.nix
+++ nix/cassandra_test.nix
@@ -14,8 +14,11 @@
       command = pkgs.writeShellApplication {
         runtimeInputs = [ cfg.package ];
         text = ''
-          nodetool -Dcom.sun.jndi.rmiURLParsing=legacy status
-          echo 'show version;' | cqlsh
+          echo "show version;" | cqlsh
+          echo "create keyspace test with replication = {'class': 'SimpleStrategy', 'replication_factor': 1};" | cqlsh
+          echo "CREATE TABLE test.test_table(id int PRIMARY KEY, name text);" | cqlsh
+          echo "insert into test.test_table (id, name) VALUES (1, 'hello');" | cqlsh
+          echo "select * from test.test_table;" | cqlsh | grep hello
         '';
         name = "cassandra-test";
       };
