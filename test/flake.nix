5e47b74baf02b764232162a7733280cd6aae87a0
shivaraj-bh <sbh69840@gmail.com>
2024-03-05 13:28:32 +0530

    test: keep package overlays in its own dir

--- test/flake.nix
+++ test/flake.nix
@@ -17,12 +17,7 @@
           inherit system;
           # Required for elastic search
           config.allowUnfree = true;
-          overlays = [
-            (final: prev: {
-              # Because tests are failing on darwin: https://github.com/juspay/services-flake/pull/115#issuecomment-1970467684
-              pgadmin4 = prev.pgadmin4.overrideAttrs (_: { doInstallCheck = false; });
-            })
-          ];
+          overlays = import ./overlays;
         };
         process-compose =
           let
