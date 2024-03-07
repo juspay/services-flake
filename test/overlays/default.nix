31151df8e4fec6dcb4631e84e4b9ed54bc34eeaa
Sridhar Ratnakumar <srid@srid.ca>
2024-03-07 14:30:08 -0500

    test(grafana): Use older nixpkgs for grafana on macOS
    This speeds up build, because we no longer have to compile grafana locally and wait for eternity.
    
    cf. https://github.com/NixOS/nixpkgs/issues/273998

--- test/overlays/default.nix
+++ test/overlays/default.nix
@@ -1,11 +1,13 @@
-{ lib, system, ... }:
+{ inputs', lib, system, ... }:
 
 let
   isDarwin = system == "x86_64-darwin" || system == "aarch64-darwin";
 in
 lib.optionals isDarwin [
   (import ./pgadmin.nix)
-  (import ./grafana.nix)
+  (self: super: {
+    grafana = inputs'.nixpkgs-grafana.legacyPackages.grafana;
+  })
 ]
 ++
 [
