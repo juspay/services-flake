f48ee3f80b6fea8303f9338fa197b61a496c8b94
Sridhar Ratnakumar <srid@srid.ca>
2024-03-11 10:14:40 -0400

    docs: rm pg2 in share-services

--- example/share-services/databases/services.nix
+++ example/share-services/databases/services.nix
@@ -17,15 +17,4 @@ in
       }
     ];
   };
-
-  services.postgres."pg2" = {
-    imports = [ globalSocket ];
-    enable = true;
-    listen_addresses = "127.0.0.1";
-    port = 5433;
-  };
-
-  # Start `pg2-init` process after `pg1-init` to avoid race condition for creating
-  # the `data` directory on the first run.
-  settings.processes."pg2-init".depends_on."pg1-init".condition = "process_completed_successfully";
 }
