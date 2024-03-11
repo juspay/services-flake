54ed8d27f824a88b8ef052660a9b194a75dcd749
Sridhar Ratnakumar <srid@srid.ca>
2024-03-11 14:06:10 -0400

    chore(example): Change branch to 'main'
    Since the PR is merged.

--- example/share-services/pgweb/flake.nix
+++ example/share-services/pgweb/flake.nix
@@ -7,8 +7,7 @@
     process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
     services-flake.url = "github:juspay/services-flake";
 
-    # TODO: Remove 'share-services' branch before merging to `main`.
-    northwind.url = "github:juspay/services-flake/share-services?dir=example/share-services/northwind";
+    northwind.url = "github:juspay/services-flake?dir=example/share-services/northwind";
   };
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
