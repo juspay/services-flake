eb4364c92a6bb8ed0534b4b6eedcd5605161fa39
Shivaraj B H <sbh69840@gmail.com>
2024-04-04 11:14:35 +0530

    fix(test): grafana on darwin is no longer broken in upstream (#161)
    fixed here: https://github.com/NixOS/nixpkgs/pull/297968
    
    Also, hydra-checks pass on all four platforms:
    
    ```sh
    shivaraj in 🌐 nixos in ~
    ❯ nix run github:nix-community/hydra-check -- grafana --arch aarch64-darwin
    Build Status for grafana.aarch64-darwin on jobset nixpkgs/trunk
    ✔ grafana-10.4.1 from 2024-03-27 - https://hydra.nixos.org/build/254421303
    
    shivaraj in 🌐 nixos in ~ took 45s
    ❯ nix run github:nix-community/hydra-check -- grafana --arch x86_64-darwin
    Build Status for grafana.x86_64-darwin on jobset nixpkgs/trunk
    ✔ grafana-10.4.1 from 2024-03-27 - https://hydra.nixos.org/build/254422747
    
    shivaraj in 🌐 nixos in ~
    ❯ nix run github:nix-community/hydra-check -- grafana --arch x86_64-linux
    Build Status for nixpkgs.grafana.x86_64-linux on jobset nixos/trunk-combined
    ✔ grafana-10.4.0 from 2024-03-27 - https://hydra.nixos.org/build/254385494
    
    shivaraj in 🌐 nixos in ~
    ❯ nix run github:nix-community/hydra-check -- grafana --arch aarch64-linux
    Build Status for nixpkgs.grafana.aarch64-linux on jobset nixos/trunk-combined
    ✔ grafana-10.4.0 from 2024-03-27 - https://hydra.nixos.org/build/254392946
    ```

--- test/nix/pkgs.nix
+++ test/nix/pkgs.nix
@@ -11,10 +11,6 @@
       overlays = [
         (self: super: lib.optionalAttrs super.stdenv.isDarwin {
 
-          # grafana is broken on aarch64-darwin, but works on older nixpkgs:
-          # https://github.com/NixOS/nixpkgs/issues/273998
-          grafana = (builtins.getFlake "github:nixos/nixpkgs/b604023e0a5549b65da3040a07d2beb29ac9fc63").legacyPackages.${system}.grafana;
-
           # Disable tests, because they are failing on darwin:
           # https://github.com/NixOS/nixpkgs/issues/281214
           pgadmin4 = super.pgadmin4.overrideAttrs (_: {
