75a42b9e9ecd34e603e482d294d4526a560e31ec
Adam Szucs-Matyas <32290917+szucsitg@users.noreply.github.com>
2024-05-07 10:36:16 +0200

    fix(nginx): link nginx.conf to dataDir (#173)

--- nix/nginx/nginx_test.nix
+++ nix/nginx/nginx_test.nix
@@ -1,18 +1,43 @@
 { pkgs, config, ... }: {
-  services.nginx."nginx1".enable = true;
+  services.nginx."nginx1" = {
+    enable = true;
+    httpConfig = ''
+      server {
+        listen 8888;  
+        include ../../importedconfig.conf;
+      }
+    '';
+  };
 
-  settings.processes.test =
-    let
-      cfg = config.services.nginx."nginx1";
-    in
+  settings.processes =
     {
-      command = pkgs.writeShellApplication {
-        runtimeInputs = [ cfg.package pkgs.gnugrep pkgs.curl ];
-        text = ''
-          curl http://127.0.0.1:${builtins.toString cfg.port} | grep -q "nginx"
-        '';
-        name = "nginx-test";
+      init = {
+        command = pkgs.writeShellApplication {
+          runtimeInputs = [ pkgs.coreutils ];
+          text = ''
+            cat >importedconfig.conf <<EOL
+            location / {
+                add_header Content-Type text/plain;
+                return 200 'Looks good';
+            }
+            EOL
+          '';
+          name = "init";
+        };
       };
-      depends_on."nginx1".condition = "process_healthy";
-    };
+      test =
+        let
+          cfg = config.services.nginx."nginx1";
+        in
+        {
+          command = pkgs.writeShellApplication {
+            runtimeInputs = [ cfg.package pkgs.gnugrep pkgs.curl ];
+            text = ''
+              curl -s  -H "Accept: text/plain" http://127.0.0.1:8888 | grep -q "Looks good"
+            '';
+            name = "nginx-test";
+          };
+          depends_on."nginx1".condition = "process_healthy";
+        };
+    } // { "nginx1".depends_on."init".condition = "process_completed"; };
 }
