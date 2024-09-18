5960a2c553ba6f5efec8d6aad64219c8fa036e50
Shivaraj B H <me@shivaraj-bh.in>
2024-09-19 00:08:59 +0530

    elasticsearch: copy plugins to `dataDir` (#331)
    resolves #325
    
    ---------
    
    Co-authored-by: Seggy Umboh <seg@seg.gy>

--- nix/services/elasticsearch_test.nix
+++ nix/services/elasticsearch_test.nix
@@ -1,11 +1,19 @@
 { pkgs, config, ... }: {
-  services.elasticsearch."es1".enable = true;
+  services.elasticsearch."es1" = {
+    enable = true;
+    plugins = [
+      pkgs.elasticsearchPlugins.analysis-icu
+    ];
+  };
   settings.processes.test =
     {
       command = pkgs.writeShellApplication {
         runtimeInputs = [ pkgs.curl ];
         text = ''
           curl 127.0.0.1:9200/_cat/health 
+
+          echo "Verify plugin installation"
+          curl 127.0.0.1:9200/_cat/plugins | grep 'analysis-icu'
         '';
         name = "elasticsearch-test";
       };
