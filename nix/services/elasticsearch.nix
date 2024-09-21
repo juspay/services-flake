79927489ad135dba85a506e9245ecdbdffeaadb4
Seggy Umboh <seg@seg.gy>
2024-09-20 17:51:28 -0700

    fix(elasticsearch): Fixed permission to add to directories in elasticsearch plugins

--- nix/services/elasticsearch.nix
+++ nix/services/elasticsearch.nix
@@ -149,7 +149,7 @@ in
                 # Install plugins
                 rm -rf "${config.dataDir}/plugins"
                 cp -rL ${esPlugins}/plugins "${config.dataDir}/plugins"
-                find "${config.dataDir}/plugins" -type d -exec chmod u+x {} \;
+                find "${config.dataDir}/plugins" -type d -exec chmod u+w {} \;
 
                 rm -f "${config.dataDir}/lib"
                 ln -sf ${config.package}/lib "${config.dataDir}/lib"
