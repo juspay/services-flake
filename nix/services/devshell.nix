72abbb7d91c27ec3a7602dbda262570c46b8be8e
shivaraj-bh <sbh69840@gmail.com>
2024-10-10 14:28:33 +0530

    chore: define services.outputs.enabledServices

--- nix/services/devshell.nix
+++ nix/services/devshell.nix
@@ -1,28 +1,53 @@
 { config, pkgs, lib, ... }:
+let
+  inherit (lib) types;
+in
 {
-  options.services.outputs.devShell = lib.mkOption {
-    type = lib.types.package;
-    readOnly = true;
-    description = ''
-      The devShell that aggregates packages from all the enabled services.
-    '';
+  options.services.outputs = {
+    enabledServices = lib.mkOption {
+      type = types.listOf types.raw;
+      readOnly = true;
+      description = ''
+        List of names of enabled services.
+
+        Each item will be of the form [ "<service>" "<instance-name>" ]
+      '';
+      example = lib.literalExpression ''
+        [ [ "mysql" "m1" ] [ "mysql" "m2" ] [ "redis" "r1" ] ]
+      '';
+    };
+    devShell = lib.mkOption {
+      type = types.package;
+      readOnly = true;
+      description = ''
+        The devShell that aggregates packages from all the enabled services.
+      '';
+    };
   };
 
   config = {
+    services.outputs.enabledServices = lib.pipe config.services [
+      # `outputs` is a reserved attribute set and is not the name of a service.
+      (lib.filterAttrs (n: _: n != "outputs"))
+
+      (lib.foldlAttrs
+        (acc: service: instances:
+          (lib.mapAttrsToList
+            (name: cfg:
+              if cfg.enable then [ service name ] else [ ]
+            )
+            instances)
+          ++ acc
+        ) [ ])
+
+      (lib.filter (enabledService: enabledService != [ ]))
+    ];
     services.outputs.devShell = pkgs.mkShell {
-      packages =
-        lib.pipe config.services [
-          # `outputs` is a reserved attribute set and is not the name of a service.
-          (lib.filterAttrs (n: _: n != "outputs"))
-          # Flatten services attrset
-          #
-          # Example:
-          # Input = { mysql."m1" = <cfg1>; mysql."m2" = <cfg2>; redis."r1" = <cfg3>; }
-          # Output = [ <cfg1> <cfg2> <cfg3> ]
-          (lib.foldlAttrs (acc: _: v: (lib.attrValues v) ++ acc) [ ])
-          (lib.filter (service: service.enable))
-          (map (service: service.package))
-        ];
+      packages = map
+        (name:
+          lib.attrByPath (name ++ [ "package" ]) (builtins.throw "${builtins.toString name} doesn't define a `package` option") config.services
+        )
+        config.services.outputs.enabledServices;
     };
   };
 }
