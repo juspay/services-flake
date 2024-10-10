b92546c0c823180c03e3a331594e5cdf9d05d8fe
shivaraj-bh <sbh69840@gmail.com>
2024-10-10 21:02:15 +0530

    refactor: clear types

--- nix/services/devshell.nix
+++ nix/services/devshell.nix
@@ -5,15 +5,16 @@ in
 {
   options.services.outputs = {
     enabledServices = lib.mkOption {
-      type = types.listOf (types.listOf types.str);
+      type = types.attrsOf (types.listOf types.str);
       readOnly = true;
       description = ''
-        List of names of enabled services.
-
-        Each item will be of the form [ "<service>" "<instance-name>" ]
+        Names of all the enabled service instances, grouped by the service name.
       '';
       example = lib.literalExpression ''
-        [ [ "mysql" "m1" ] [ "mysql" "m2" ] [ "redis" "r1" ] ]
+        {
+          mysql = [ "m1" "m2" ];
+          redis = [ "r1" ];
+        }
       '';
     };
     devShell = lib.mkOption {
@@ -30,24 +31,20 @@ in
       # `outputs` is a reserved attribute set and is not the name of a service.
       (lib.filterAttrs (n: _: n != "outputs"))
 
-      (lib.foldlAttrs
-        (acc: service: instances:
-          (lib.mapAttrsToList
-            (name: cfg:
-              if cfg.enable then [ service name ] else [ ]
-            )
-            instances)
-          ++ acc
-        ) [ ])
-
-      (lib.filter (enabledService: enabledService != [ ]))
+      (lib.mapAttrs (_service: instances:
+        lib.attrNames (lib.filterAttrs (_: v: v.enable) instances)))
     ];
+
     services.outputs.devShell = pkgs.mkShell {
-      packages = map
-        (name:
-          lib.attrByPath (name ++ [ "package" ]) (builtins.throw "${builtins.toString name} doesn't define a `package` option") config.services
-        )
-        config.services.outputs.enabledServices;
+      packages = lib.pipe config.services.outputs.enabledServices [
+        (lib.mapAttrsToList (service: instances:
+          map
+            (instance:
+              lib.attrByPath ([ service instance "package" ]) (builtins.throw "${service}.${instance} doesn't define a `package` option") config.services)
+            instances))
+
+        lib.flatten
+      ];
     };
   };
 }
