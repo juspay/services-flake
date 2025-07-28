88aa0da4134fbcb128aef80f6d0e522888230428
shivaraj-bh <sbh69840@gmail.com>
2025-07-28 15:37:07 +0530

    chore(test): Disable `apache-kafka` test
    Test error logs:
    ```sh
    apache-kafka-test> [k1  ] [2025-07-28 10:07:44,360] ERROR Exiting Kafka
    due to fatal exception (kafka.Kafka$)
    apache-kafka-test> [k1  ]
    org.apache.kafka.common.config.ConfigException: Missing required
    configuration "process.roles" which has no default value.
    apache-kafka-test> [k1  ]       at
    org.apache.kafka.common.config.ConfigDef.parseValue(ConfigDef.java:534)
    ~[kafka-clients-4.0.0.jar:?]
    apache-kafka-test> [k1  ]       at
    org.apache.kafka.common.config.ConfigDef.parse(ConfigDef.java:524)
    ~[kafka-clients-4.0.0.jar:?]
    apache-kafka-test> [k1  ]       at
    org.apache.kafka.common.config.AbstractConfig.<init>(AbstractConfig.java:115)
    ~[kafka-clients-4.0.0.jar:?]
    apache-kafka-test> [k1  ]       at
    org.apache.kafka.server.config.AbstractKafkaConfig.<init>(AbstractKafkaConfig.java:72)
    ~[kafka-server-4.0.0.jar:?]
    apache-kafka-test> [k1  ]       at
    kafka.server.KafkaConfig.<init>(KafkaConfig.scala:156)
    ~[kafka_2.13-4.0.0.jar:?]
    apache-kafka-test> [k1  ]       at
    kafka.server.KafkaConfig.<init>(KafkaConfig.scala:159)
    ~[kafka_2.13-4.0.0.jar:?]
    apache-kafka-test> [k1  ]       at
    kafka.Kafka$.buildServer(Kafka.scala:65) ~[kafka_2.13-4.0.0.jar:?]
    apache-kafka-test> [k1  ]       at kafka.Kafka$.main(Kafka.scala:75)
    [kafka_2.13-4.0.0.jar:?]
    apache-kafka-test> [k1  ]       at kafka.Kafka.main(Kafka.scala)
    [kafka_2.13-4.0.0.jar:?]
    
    ```
    
    apache-kafka no longer works with zookeeper, see
    https://www.confluent.io/blog/latest-apache-kafka-release/
    
    Add the test back after resolving
    https://github.com/juspay/services-flake/issues/395

--- test/flake.nix
+++ test/flake.nix
@@ -41,7 +41,6 @@
               };
           in
           builtins.listToAttrs (builtins.map mkPackageFor ([
-            "${inputs.services-flake}/nix/services/apache-kafka_test.nix"
             "${inputs.services-flake}/nix/services/cassandra_test.nix"
             "${inputs.services-flake}/nix/services/clickhouse/clickhouse_test.nix"
             "${inputs.services-flake}/nix/services/grafana_test.nix"
