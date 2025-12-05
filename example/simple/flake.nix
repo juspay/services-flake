7152e7c15fc9011ad80f411fd9e35632f0f37ee0
shivaraj-bh <me@shivaraj-bh.in>
2025-12-05 11:45:03 +0530

    chore: switch back to main

--- example/simple/flake.nix
+++ example/simple/flake.nix
@@ -4,7 +4,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
-    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake/exec-wd";
+    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
     services-flake.url = "github:juspay/services-flake";
 
     northwind.url = "github:pthom/northwind_psql";
