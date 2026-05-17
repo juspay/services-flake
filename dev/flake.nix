b0012dbf955efc3f6e2decd61610da5cea17dce6
Shivaraj B H <me@shivaraj-bh.in>
2026-05-18 00:00:32 +0530

    chore(dev): remove unused flake inputs (#682)

--- dev/flake.nix
+++ dev/flake.nix
@@ -2,10 +2,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-root.url = "github:srid/flake-root";
     pre-commit-hooks-nix.url = "github:cachix/pre-commit-hooks.nix";
     pre-commit-hooks-nix.flake = false;
-    cachix-push.url = "github:juspay/cachix-push";
 
     # CI will override `services-flake` to run checks on the latest source
     services-flake.url = "github:juspay/services-flake";
@@ -14,18 +12,10 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [
-        inputs.flake-root.flakeModule
         (inputs.pre-commit-hooks-nix + /flake-module.nix)
-        inputs.cachix-push.flakeModule
         ./nix/pre-commit.nix
       ];
       perSystem = { self', pkgs, config, ... }: {
-        cachix-push = {
-          cacheName = "services-flake";
-          pathsToCache = {
-            devshell = self'.devShells.default;
-          };
-        };
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             just
