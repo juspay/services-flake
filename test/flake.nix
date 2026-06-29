ad377f58fae022cba2bc9b55018de33de6094dca
shivaraj-bh <me@shivaraj-bh.in>
2026-06-29 05:11:55 +0000

    test: on darwin fails for now

--- test/flake.nix
+++ test/flake.nix
@@ -62,7 +62,6 @@
             "${inputs.services-flake}/nix/services/neo4j_test.nix"
             "${inputs.services-flake}/nix/services/redis_test.nix"
             "${inputs.services-flake}/nix/services/redis-cluster_test.nix"
-            "${inputs.services-flake}/nix/services/seaweedfs_test.nix" # Darwin build fixed in https://github.com/NixOS/nixpkgs/pull/534897
             "${inputs.services-flake}/nix/services/searxng_test.nix"
             "${inputs.services-flake}/nix/services/tempo_test.nix"
             "${inputs.services-flake}/nix/services/loki_test.nix"
@@ -90,6 +89,7 @@
             #   "aarch64-darwin"
             # ]
             "${inputs.services-flake}/nix/services/open-webui_test.nix"
+            "${inputs.services-flake}/nix/services/seaweedfs_test.nix" # Darwin build fixed in https://github.com/NixOS/nixpkgs/pull/534897
           ]
           # Tests on non-linux host only
           ++ lib.optionals (!pkgs.stdenv.hostPlatform.isLinux) [
